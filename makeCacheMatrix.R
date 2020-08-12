c1fb819ccf7fc1230b691a8a138eb3b7f4aaa779
Arnav Das <68970284+Arnav-10@users.noreply.github.com>
2020-08-12 16:38:30 +0530

    Add files via upload

---
+++ makeCacheMatrix.R
@@ -0,0 +1,27 @@
+makeCacheMatrix <- function(x = matrix()) { 
+  inv <- NULL                            
+  set <- function(y) {                   
+    x <<- y
+    inv <<- NULL                       
+  }
+  get <- function() x                    
+  
+  setinverse <- function(inverse) inv <<- inverse  ## assigns value of inv in parent environment
+  getinverse <- function() inv                     ## gets the value of inv where called
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)  ## you need this in order to refer 
+  ## to the functions with the $ operator
+}
+
+
+cacheSolve <- function(x, ...) {
+  ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinverse()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setinverse(inv)
+  inv
+}
